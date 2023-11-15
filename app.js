2a43c52b96b2cc4c52115fd240981a71fabfa401
Eric Huie <erichuie97@gmail.com>
2023-11-14 16:59:40 -0800

    Finished step1 getting data from Giphy

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+"use strict";
 console.log("Let's get this party started!");
+
+async function getGiphy(event){
+  event.preventDefault();
+  console.log("test");
+  const searchTerm = $("#searchBox").val();
+  const params = new URLSearchParams({
+    "q": searchTerm,
+    "api_key": "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  });
+  const fetchResponse = await fetch(`http://api.giphy.com/v1/gifs/search?${params}`);
+  const fetchText = await fetchResponse.text();
+  console.log("type= ", typeof(fetchText));
+  console.log("response= ", fetchText);
+}
+
+$("form").on("submit", getGiphy);
